298d6608b1d2a9f1d89df7f3c201a277d471a104
Matt Eva <matteweva@gmail.com>
2021-12-22 09:15:35 -0800

    commit tree f2473e534070e29ca6d777c94cec797abac52444

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable
+
+private 
+
+  def not_found(exception)
+    render json: {error: "#{exception.model} not found"}, status: :not_found
+  end
+
+  def unprocessable(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def authorize
+    return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+  end
 
 end
